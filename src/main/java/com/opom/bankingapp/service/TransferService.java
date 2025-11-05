2c17e55679523f36f2b4040e35bba8834664757c
thanthtooaung <thanthtoo1285@gmail.com>
2025-11-05 23:00:18 +0700

    :sparkles: feat: added /transfer/validte API

--- src/main/java/com/opom/bankingapp/service/TransferService.java
+++ src/main/java/com/opom/bankingapp/service/TransferService.java
@@ -1,10 +1,9 @@
 package com.opom.bankingapp.service;
 
-import com.opom.bankingapp.dto.transfer.AccountNumberPrepareRequest;
-import com.opom.bankingapp.dto.transfer.NicknamePrepareRequest;
-import com.opom.bankingapp.dto.transfer.TransferPrepareResponse;
+import com.opom.bankingapp.dto.transfer.*;
 
 public interface TransferService {
     TransferPrepareResponse prepareNicknameTransfer(Long fromUserId, NicknamePrepareRequest request);
     TransferPrepareResponse prepareAccountNumberTransfer(AccountNumberPrepareRequest request);
+    ValidateTransferResponse validateTransfer(Long fromUserId, ValidateTransferRequest request);
 }
--- src/main/java/com/opom/bankingapp/service/impl/TransferServiceImpl.java
+++ src/main/java/com/opom/bankingapp/service/impl/TransferServiceImpl.java
@@ -1,11 +1,14 @@
 package com.opom.bankingapp.service.impl;
 
-import com.opom.bankingapp.dto.transfer.AccountNumberPrepareRequest;
-import com.opom.bankingapp.dto.transfer.NicknamePrepareRequest;
-import com.opom.bankingapp.dto.transfer.TransferPrepareResponse;
+import com.opom.bankingapp.dto.transfer.*;
+import com.opom.bankingapp.dto.user.AccountDetailResponse;
 import com.opom.bankingapp.exception.ResourceNotFoundException;
+import com.opom.bankingapp.repository.AccountRepository;
 import com.opom.bankingapp.repository.TransferRepository;
+import com.opom.bankingapp.repository.UserRepository;
 import com.opom.bankingapp.service.TransferService;
+import com.opom.bankingapp.service.UserService;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +16,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class TransferServiceImpl implements TransferService {
 
     private final TransferRepository transferRepository;
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
 
-    public TransferServiceImpl(TransferRepository transferRepository) {
+    public TransferServiceImpl(TransferRepository transferRepository,
+                               UserService userService,
+                               UserRepository userRepository,
+                               AccountRepository accountRepository) {
         this.transferRepository = transferRepository;
+        this.userService = userService;
+        this.userRepository = userRepository;
+        this.accountRepository = accountRepository;
     }
 
     @Override
@@ -35,5 +47,23 @@ public class TransferServiceImpl implements TransferService {
         return transferRepository.findAccountPrepareDetails(
                 request.toAccountNumber()
         ).orElseThrow(() -> new ResourceNotFoundException("Account not found"));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ValidateTransferResponse validateTransfer(Long fromUserId, ValidateTransferRequest request) {
+        userService.verifyPin(fromUserId, request.pin());
+
+        Long fromAccountId = userRepository.findSelectedAccountIdByUserId(fromUserId)
+                .orElseThrow(() -> new BadCredentialsException("No account selected. Please select an account to transfer from."));
+
+        AccountDetailResponse fromAccountDetails = accountRepository.findAccountDetailsById(fromAccountId)
+                .orElseThrow(() -> new ResourceNotFoundException("From-account details not found for ID: " + fromAccountId));
+
+        long toAccountId = request.toAccountId();
+        AccountDetailResponse toAccountDetails = accountRepository.findAccountDetailsById(toAccountId)
+                .orElseThrow(() -> new ResourceNotFoundException("To-account not found with ID: " + toAccountId));
+
+        return new ValidateTransferResponse(fromAccountDetails, toAccountDetails);
     }
 }
