d38ef3de58d72eae10ebc501ceb694923a31b874
thanthtooaung <thanthtoo1285@gmail.com>
2025-11-08 14:40:16 +0700

    :recycle: fix: fixed /users/me API #2

---
+++ src/main/java/com/opom/bankingapp/dto/user/ProfileDetailsDto.java
@@ -0,0 +1,14 @@
+package com.opom.bankingapp.dto.user;
+
+import com.opom.bankingapp.dto.common.OptionDto;
+import java.time.LocalDate;
+
+public record ProfileDetailsDto(
+    String fullname,
+    LocalDate dateOfBirth,
+    OptionDto gender,
+    OptionDto nationality,
+    boolean isPolicyAgreement,
+    boolean isAutoSaveReceipt,
+    Long selectedAccountId
+) {}
